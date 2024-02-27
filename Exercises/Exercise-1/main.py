3d00164bacdf851acac1d4ccdfbee9e41d388766
Casper ten Dam <caspertd97@gmail.com>
2024-02-27 13:24:14 +0100

    Implement first version of file download function. Add structlog to requirements.

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,9 @@
+import re 
 import requests
+import pathlib
+import structlog 
+
+logger = structlog.get_logger()
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -11,9 +16,32 @@ download_uris = [
 ]
 
 
+def download_files(target_dir: str | pathlib.Path = pathlib.Path("downloads")):
+    logger.info(f"Downloading {len(download_uris)} files")
+    if isinstance(target_dir, str):
+        target_dir = pathlib.Path(target_dir)
+    # Check if the target directory exists, else, create it
+    if not target_dir.exists():
+        target_dir.mkdir()
+    for download_uri in download_uris:
+        download_file(download_uri, target_dir)
+
+def extract_filename(download_uri: str) -> str:
+    return download_uri.split("/")[-1].split(".")[0]
+
+def download_file(download_uri: str, target_dir: pathlib.Path) -> None: 
+    # Extract the filename: 
+    filename = extract_filename(download_uri)
+    # (Attempt to) download the file
+    logger.info(f"Downloading file {filename}")
+    r = requests.get(download_uri, allow_redirects=True)
+    if r.ok:
+        open(target_dir / filename, 'wb').write(r.content)
+    else: 
+        logger.warning(f"HTTP request for {filename} failed with status code {r.status_code}.")
+
 def main():
-    # your code here
-    pass
+    download_files(pathlib.Path("downloads"))
 
 
 if __name__ == "__main__":
