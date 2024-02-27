be340b477a20573a6a08e9512a17b93e0d570ecc
Casper ten Dam <caspertd97@gmail.com>
2024-02-27 16:05:02 +0100

    Implement extracting and deleting zip file.

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -2,7 +2,8 @@ import re
 import requests
 import pathlib
 import structlog 
-
+from zipfile import ZipFile 
+  
 logger = structlog.get_logger()
 
 download_uris = [
@@ -24,21 +25,34 @@ def download_files(target_dir: str | pathlib.Path = pathlib.Path("downloads")):
     if not target_dir.exists():
         target_dir.mkdir()
     for download_uri in download_uris:
-        download_file(download_uri, target_dir)
+        zip_file = download_file(download_uri, target_dir)
+        if zip_file is None: 
+            continue 
+        extract_and_delete_zip_file(zip_file, target_dir) 
+
 
 def extract_filename(download_uri: str) -> str:
     return download_uri.split("/")[-1]
 
-def download_file(download_uri: str, target_dir: pathlib.Path) -> None: 
+def download_file(download_uri: str, target_dir: pathlib.Path) -> pathlib.Path | None: 
     # Extract the filename: 
     filename = extract_filename(download_uri)
     # (Attempt to) download the file
     logger.info(f"Downloading file {filename}")
     r = requests.get(download_uri, allow_redirects=True)
     if r.ok:
-        open(target_dir / filename, 'wb').write(r.content)
+        zip_file = target_dir / filename
+        open(zip_file, 'wb').write(r.content)
+        return zip_file
     else: 
         logger.warning(f"HTTP request for {filename} failed with status code {r.status_code}.")
+        return None
+
+def extract_and_delete_zip_file(zip_file: pathlib.Path, target_dir: pathlib.Path) -> None: 
+    logger.info(f"Extracting and deleting {zip_file}.")
+    with ZipFile(zip_file, 'r') as zObject:
+        zObject.extractall(path=target_dir)
+    zip_file.unlink() # remove the zip file
 
 def main():
     download_files(pathlib.Path("downloads"))
